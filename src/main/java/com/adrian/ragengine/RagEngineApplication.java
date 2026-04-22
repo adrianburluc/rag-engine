9155cbfb56074bd58957e4583c32c3ced1dd1ebd
Adrian Burluc <adrianburluc@gmail.com>
2026-04-22 15:57:09 +0300

    Initial commit

---
+++ src/main/java/com/adrian/ragengine/RagEngineApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.ragengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrian/ragengine/RagEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.ragengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
